d747568d00a3ab569b44e5bfa1b3d07e40fec962
Ramchenna B <96220805+RamchennaB@users.noreply.github.com>
2024-04-20 22:29:41 +0530

    Delete scrap.py

--- scrap.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-req = requests.get("https://cs.fyi/guide/how-does-internet-work")
-soup = BeautifulSoup(req.content, "html.parser")
-file = open("Scrapeddata.txt", "w+", encoding="utf-8")
-file.write(soup.prettify()) 
-print("file created successfully")
